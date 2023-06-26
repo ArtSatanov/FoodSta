3e02e96e3b3005b4a9b12fd5789107587d323ab2
ArtSatanov <artsatanov@gmail.com>
2023-06-26 22:29:02 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,10 +1,46 @@
-function findLongestWord(string) {
-  // Change code below this line
+// function findLongestWord(string) {
+//   // Change code below this line
 
-  let result = 0;
-  for (i = 0; i <= number; i += 1) {
-    result += i;
-    // Change code above this line
-  }
-  return result;
-}
+//   const array = string.split(' ');
+//   let longestWord = '0';
+//   let indexId = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i].length > longestWord.length) {
+//       console.log(array[i].length, array[i]);
+//       console.log(longestWord);
+//       indexId = i;
+    
+//       longestWord = array[indexId];
+//       console.log(longestWord);
+//     }
+    
+//     // Change code above this line
+    
+//   }
+//   return longestWord;
+// }
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = 0; i <= max - min; i += 1 )
+//     numbers.push(min + i);
+//   // Change code above this line
+//   return numbers;
+// }
+
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//   const array = [];
+//   let elementId = 0;
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (Number(numbers[i]) > Number(value)) {
+//       console.log(numbers[i], value, i);
+//       elementId = i;
+//       array.push(numbers[elementId]);
+//     }
+//   }
+
+//   return array;
+//   // Change code above this line
+// }
