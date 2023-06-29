e4e7b14825c37f329818e497bf5968f681c2e4ad
ArtSatanov <artsatanov@gmail.com>
2023-06-29 23:25:00 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -44,3 +44,70 @@
 //   return array;
 //   // Change code above this line
 // }
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   const array3 = [];
+//  for (let i = 0; i <= array1.length; i+= 1) {
+//   if (array2.includes(array1[i])){
+//      array3.push(array1[i]);
+//      console.log(array1[i]);
+//   }
+   
+//  }
+   
+// return array3;
+
+//  // Change code above this line
+// }
+// function calculateTotalPrice(order) {
+//   let total2 = 0;
+//   // // Change code below this line
+
+//   // for (let i = 0; i < order.length; i += 1) {
+//   //   total += order[i];
+//   // }
+
+//   for (let total of order) {
+//      total += 0;
+//      total2 += total;
+//      console.log(total,total2);
+//   }
+
+//   // Change code above this line
+//   return total2;
+// }
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+//   const numbers = [];
+// for (i = start; i <= end; i += 1) {
+//   if (i % 2 === 0 ) {
+//     numbers.push(i);
+//   }
+// }
+
+// return numbers;
+//     // Change code above this line
+//   }
+
+function includes(array, value) {
+   // Change code below this line
+   for (i = 0; i < array.length; i += 1) {
+      if (array[i] === value) {
+         return true;
+      }
+      // Change code above this line
+   }
+   return false;
+}
+
+function includes(array, value) {
+   // Change code below this line
+   for (let number of array) {
+      if (number === value) {
+         return true;
+      }
+      // Change code above this line
+   }
+   return false;
+}
