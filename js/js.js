acf3c5182b40807fca029fe64683657b3cdf69c6
ArtSatanov <artsatanov@gmail.com>
2023-07-02 15:09:55 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -236,3 +236,22 @@
 //    }
 //    return arrayResult;
 // }
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+// let totalCost = 0;
+
+//   for (let product of products) {
+//      if (product.name === productName) {
+//         totalCost = product.price * product.quantity;
+//      }
+//   }
+//    return totalCost;
+//   // Change code above this line
+// }
